d2943beec06a64d0b080a6e1cb1c9364059b00d9
AdiltTeam <adilteam313@gmail.com>
2025-04-12 02:27:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-from app import app
+from app import app  # app.py faylından Flask tətbiqini idxal et
 
 if __name__ == "__main__":
-    # Get port from environment or use default
-    port = int(127.0.0.1("PORT", 5000))
+    # Portu mühit dəyişənindən al, yoxsa 5000 istifadə et
+    port = int(os.environ.get("PORT", 5000))  # PORT mühit dəyişəni, 5000 default portdur
     app.run(host="0.0.0.0", port=port)
